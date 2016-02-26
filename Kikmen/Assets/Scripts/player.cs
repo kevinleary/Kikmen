738e9778d96c55b5e67e408527e970a39cd4ebcf
kevinleary <kevin.n.leary@gmail.com>
2016-02-26 18:04:37 -0500

    0.1c
    +Added Cube Spawner
    +Added Non-Working Jumping
    ~Need to keep working on that
    +Changes to Scripts

---
+++ Kikmen/Assets/Scripts/player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+
+    // public Vector3 movement;
+    public float speed = 5f;
+    public float jumpspeed = .5f;
+    //public Rigidbody rb;
+    public bool OnGround = true;
+    public bool isAirborne = false;
+    // Use this for initialization
+    void Start()
+    {
+
+        //rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        if (OnGround)                       //testing out jumping not working very well
+        {
+            if (Input.GetButton("Jump"))
+            {
+                transform.Translate(0f, jumpspeed , 0f);
+                print("Youre Jumping");
+                //isAirborne = true;
+                OnGround = false;
+            }
+
+            if (!OnGround && !Input.GetButton("Jump"))
+            {
+                transform.Translate(0f, -jumpspeed , 0f);
+                print("Youre falling");
+            }
+            //  else
+            //    OnGround = false;
+        }
+    }
+}
---
+++ Kikmen/Assets/Scripts/spawncubes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawncubes : MonoBehaviour {
+
+    public Transform prefab;
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < 2; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 1, 2), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
