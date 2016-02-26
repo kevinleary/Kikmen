8ce6c9c17eaa888169647b65e9e247efacdfc607
kevinleary <kevin.n.leary@gmail.com>
2016-02-26 01:57:01 -0500

    0.1b
    +Changed Terrain for Plane
    +Added Material Colors
    +Added Rigidbody Test Cube
    +Added Simple movement!
    +Revised Camera angle some more

--- Kikmen/Assets/Scripts/Movement.cs
+++ Kikmen/Assets/Scripts/Movement.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    
-	// Use this for initialization
-	void Start () {
+   // public Vector3 movement;
+    public float speed = 5f;
+    //public Rigidbody rb;
 
-        Vector3
-       
-	}
+    // Use this for initialization
+    void Start () {
+
+        //rb = GetComponent<Rigidbody>();
+
+     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetButton("D"))
-        {
-            
-        }
-	}
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+        
+    }
 }
