2cb1ef058c46f08980c8222bc134f069a5fc8436
kevinleary <kevin.n.leary@gmail.com>
2016-02-25 19:12:12 -0500

    0.1a
    +Added Character
    +Added Camera tracking
    +Added terrain

---
+++ Kikmen/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+
+        Vector3
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("D"))
+        {
+            
+        }
+	}
+}
